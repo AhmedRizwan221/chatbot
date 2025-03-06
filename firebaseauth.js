333c2654177c78cb25e635be0bae55db9c4f29a1
AhmedRizwan221 <gujjarahmedrizwan@gmial.com>
2025-03-06 23:43:22 +0500

    bom bom

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,22 +1,26 @@
 import {auth} from "./firebaseconfig.js";
 import {onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
 
+// Check if user is logged in or not
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is logged in, redirect to chatbot page
+        if (window.location.pathname.includes("login.html")) {
+            window.location.href = "index.html";
+        }
+    } else {
+        // User is not logged in, redirect to login page
+        if (!window.location.pathname.includes("login.html")) {
+            window.location.href = "login.html";
+        }
+    }
+});
 
-const auth = getAuth();
-
-// Check if the user is logged in
-export const isUserLoggedIn = () => {
-  return new Promise((resolve) => {
-    onAuthStateChanged(auth, (user) => resolve(!!user));
-  });
-};
-
-// Get current user
-export const getCurrentUser = () => {
-  return auth.currentUser;
-};
-
-
+// Logout function (Add this inside a logout button in index.html)
+document.getElementById("logout-btn")?.addEventListener("click", async () => {
+    await signOut(auth);
+    window.location.href = "login.html"; // Redirect to login page
+});
 
 
 
